13023b1d86a8ce224f4177e263e04f38e8a23abd
alexrayo <yutarunet@gmail.com>
2026-04-30 17:04:25 -0600

    add google map view by branch office

--- src/data/homeData.ts
+++ src/data/homeData.ts
@@ -25,15 +25,69 @@ export const requirementsHtml = `
 
 // SUCURSALES
 export const branches = [
-  { city: 'Estelí', title: 'Estelí', address: 'Edificio Gonzáles 25vrs. al sur, segunda planta.', phone: '8338 9181' },
-  { city: 'Juigalpa', title: 'Juigalpa', address: 'Banpro 1 ½ al sur.', phone: '8516 6426' },
-  { city: 'Managua Bolonia', title: 'Managua Bolonia', address: 'Reparto Bolonia del Portón Principal del donde fue el hospital militar 1 cuadra al lago 1 ½ abajo.', phone: '7872 3485' },
-  { city: 'Managua Norte', title: 'Managua Norte', address: 'Barrio Romín Manrique costado oeste de los semáforos de la Tenderí.', phone: '7553 4506' },
-  { city: 'Rivas', title: 'Rivas', address: 'Barrio Santa Ana Costado Norte Gimnasio Humberto Méndez.', phone: '8602 4655' },
-  { city: 'Sébaco', title: 'Sébaco', address: 'BDF 3 cuadras al Este.', phone: '8606 8189' },
-  { city: 'León', title: 'Leon', address: 'Barrio El Sagrario frente costado Noreste de la UNAN-León.', phone: '7872 3944' },
-  { city: 'Masaya', title: 'Masaya', address: 'Barrio Mauricio Montenegro de Restaurante Verdi 75 varas al sur.', phone: '7872 3497' },
-  { city: 'Jinotepe', title: 'Jinotepe', address: 'San Antonio, Frente al Costado oeste de donde fueron los Juzgados.', phone: '7872 3509' },
+  {
+    city: 'Estelí',
+    title: 'Estelí',
+    address: 'Edificio Gonzáles 25vrs. al sur, segunda planta.',
+    phone: '8338 9181',
+    coords: { lat: 13.092248, lng: -86.356702 }
+  },
+  {
+    city: 'Juigalpa',
+    title: 'Juigalpa',
+    address: 'Banpro 1 ½ al sur.',
+    phone: '8516 6426',
+    coords: { lat: 12.104718289069293, lng: -85.36467933348358 }
+  },
+  {
+    city: 'Managua Bolonia',
+    title: 'Managua Bolonia',
+    address: 'Reparto Bolonia del Portón Principal del donde fue el hospital militar 1 cuadra al lago 1 ½ abajo.',
+    phone: '7872 3485',
+    coords: { lat: 12.140, lng: -86.280 },
+  },
+  {
+    city: 'Managua Norte',
+    title: 'Managua Norte',
+    address: 'Barrio Romín Manrique costado oeste de los semáforos de la Tenderí.',
+    phone: '7553 4506',
+    coords: { lat: 12.145, lng: -86.253 },
+  },
+  {
+    city: 'Rivas',
+    title: 'Rivas',
+    address: 'Barrio Santa Ana Costado Norte Gimnasio Humberto Méndez.',
+    phone: '8602 4655',
+    coords: { lat: 11.440, lng: -85.831 },
+  },
+  {
+    city: 'Sébaco',
+    title: 'Sébaco',
+    address: 'BDF 3 cuadras al Este.',
+    phone: '8606 8189',
+    coords: { lat: 12.851679, lng: -86.096372 }
+  },
+  {
+    city: 'León',
+    title: 'Leon',
+    address: 'Barrio El Sagrario frente costado Noreste de la UNAN-León.',
+    phone: '7872 3944',
+    coords: { lat: 12.43729894126253, lng: -86.87844989681538 }
+  },
+  {
+    city: 'Masaya',
+    title: 'Masaya',
+    address: 'Barrio Mauricio Montenegro de Restaurante Verdi 75 varas al sur.',
+    phone: '7872 3497',
+    coords: { lat: 11.983589, lng: -86.096922 }
+  },
+  {
+    city: 'Jinotepe',
+    title: 'Jinotepe',
+    address: 'San Antonio, Frente al Costado oeste de donde fueron los Juzgados.',
+    phone: '7872 3509',
+    coords: { lat: 11.850, lng: -86.200 },
+  }
 ];
 
 // HOME CARDS
